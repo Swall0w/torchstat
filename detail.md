52d0288a1bde42a3a265cd9945119e384ffee04f
Swall0w <technext.jpn@gmail.com>
2018-11-01 23:29:29 +0900

    [add]: BatchNorm2d and Linear for Memory.

--- detail.md
+++ detail.md
@@ -1,15 +1,15 @@
 # Supported Layers
-|Layer|Flops|Madd|
-|---|---|---|
-|Conv2d|ok|ok|
-|ConvTranspose2d| |ok|
-|BatchNorm2d|ok|ok|
-|Linear|ok|ok|
-|UpSample|ok| |
-|AvgPool2d|ok|ok|
-|MaxPool2d|ok|ok|
-|ReLU|ok|ok|
-|ReLU6|ok|ok|
-|LeaklyReLU|ok||
-|PReLU|ok|ok|
-|ELU|ok|ok|
+|Layer|Flops|Madd|MemRead|MemWrite|
+|---|---|---|---|---| 
+|Conv2d|ok|ok|ok| ok|
+|ConvTranspose2d| |ok|||
+|BatchNorm2d|ok|ok|ok|ok|
+|Linear|ok|ok|ok|ok|
+|UpSample|ok| |||
+|AvgPool2d|ok|ok|||
+|MaxPool2d|ok|ok|||
+|ReLU|ok|ok|ok|ok|
+|ReLU6|ok|ok|ok|ok|
+|LeaklyReLU|ok||ok|ok|
+|PReLU|ok|ok|||
+|ELU|ok|ok|ok|ok|
--- torchstat/compute_memory.py
+++ torchstat/compute_memory.py
@@ -8,10 +8,18 @@ def compute_memory(module, inp, out):
         return compute_ReLU_memory(module, inp, out)
     elif isinstance(module, nn.Conv2d):
         return compute_Conv2d_memory(module, inp, out)
+    elif isinstance(module, nn.BatchNorm2d):
+        return compute_BatchNorm2d_memory(module, inp, out)
+    elif isinstance(module, nn.Linear):
+        return compute_Linear_memory(module, inp, out)
     else:
         print(f"[Memory]: {type(module).__name__} is not supported!")
         return (0, 0)
     pass
+
+
+def num_params(module):
+    return sum(p.numel() for p in module.parameters() if p.requires_grad)
 
 
 def compute_ReLU_memory(module, inp, out):
@@ -33,6 +41,26 @@ def compute_Conv2d_memory(module, inp, out):
     out_c, out_h, out_w = out.size()[1:]
 
     # This includes weighs with bias if the module contains it.
-    mread = inp.size().numel() + sum(p.numel() for p in module.parameters() if p.requires_grad)
+    mread = batch_size * (inp.size()[1:].numel() + num_params(module))
     mwrite = batch_size * out_c * out_h * out_w
+    return (mread, mwrite)
+
+
+def compute_BatchNorm2d_memory(module, inp, out):
+    assert isinstance(module, nn.BatchNorm2d)
+    assert len(inp.size()) == 4 and len(inp.size()) == len(out.size())
+    batch_size, in_c, in_h, in_w = inp.size()
+
+    mread = batch_size * (inp.size()[1:].numel() + 2 * in_c)
+    mwrite = inp.size().numel()
+    return (mread, mwrite)
+
+
+def compute_Linear_memory(module, inp, out):
+    assert isinstance(module, nn.Linear)
+    assert len(inp.size()) == 2 and len(out.size()) == 2
+    batch_size = inp.size()[0]
+    mread = batch_size * (inp.size()[1:].numel() + num_params(module))
+    mwrite = out.size().numel()
+
     return (mread, mwrite)
